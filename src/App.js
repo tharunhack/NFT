b5f9f3049b21f72a81b97e3ae692f32f567adefd
tharun <88941965+tharunhack@users.noreply.github.com>
2023-05-10 17:21:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,517 @@
+import './App.css';
+import {useState} from 'react'
+import lighthouse from '@lighthouse-web3/sdk';
+import React from 'react';
+import {ethers } from "ethers";
+
+function App() {
+  const[images,setImages] = useState([]);
+  const[name,setName] = useState("");
+  const[des,setDes] = useState("");
+  const[recipt,setRecipt] = useState("")
+
+  const NFTAbi = [
+    {
+      "inputs": [],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "owner",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "approved",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "uint256",
+          "name": "tokenId",
+          "type": "uint256"
+        }
+      ],
+      "name": "Approval",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "owner",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "operator",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "bool",
+          "name": "approved",
+          "type": "bool"
+        }
+      ],
+      "name": "ApprovalForAll",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "previousOwner",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "OwnershipTransferred",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "uint256",
+          "name": "tokenId",
+          "type": "uint256"
+        }
+      ],
+      "name": "Transfer",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "tokenId",
+          "type": "uint256"
+        }
+      ],
+      "name": "approve",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "owner",
+          "type": "address"
+        }
+      ],
+      "name": "balanceOf",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "tokenId",
+          "type": "uint256"
+        }
+      ],
+      "name": "getApproved",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "owner",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "operator",
+          "type": "address"
+        }
+      ],
+      "name": "isApprovedForAll",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "recipient",
+          "type": "address"
+        },
+        {
+          "internalType": "string",
+          "name": "tokenURI",
+          "type": "string"
+        }
+      ],
+      "name": "mintNFT",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "name",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "owner",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "tokenId",
+          "type": "uint256"
+        }
+      ],
+      "name": "ownerOf",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "renounceOwnership",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "tokenId",
+          "type": "uint256"
+        }
+      ],
+      "name": "safeTransferFrom",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "tokenId",
+          "type": "uint256"
+        },
+        {
+          "internalType": "bytes",
+          "name": "data",
+          "type": "bytes"
+        }
+      ],
+      "name": "safeTransferFrom",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "operator",
+          "type": "address"
+        },
+        {
+          "internalType": "bool",
+          "name": "approved",
+          "type": "bool"
+        }
+      ],
+      "name": "setApprovalForAll",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "bytes4",
+          "name": "interfaceId",
+          "type": "bytes4"
+        }
+      ],
+      "name": "supportsInterface",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "symbol",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "tokenId",
+          "type": "uint256"
+        }
+      ],
+      "name": "tokenURI",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "tokenId",
+          "type": "uint256"
+        }
+      ],
+      "name": "transferFrom",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "transferOwnership",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ]
+  const handleChangeName = (event) => {
+    setName(event.target.value)
+  }
+
+  const handleChangeDes = (event) => {
+    setDes(event.target.value)
+
+  }
+ 
+  const uploadImage = async(e) => {
+    if(images === images){
+      const wait = alert("are you sure to continue the process")
+      const output = await lighthouse.upload(e, "LIGHTHOUSE APIKEY");
+      console.log('File Status:', output);
+
+      let images = 'https://gateway.lighthouse.storage/ipfs/' + output.data.Hash;
+      setImages(images)
+    
+    }else{
+      alert("you didn't uploaded the image")
+    }
+  }
+  
+  const changeSubmit = async(e) => {
+    e.preventDefault();
+    if(!name||!des||!images){alert("you didn't enter the details properly")
+    return }
+      let metadata = {
+        name:name,
+        description:des,
+        image:images
+      }
+      let metadataStringified = JSON.stringify(metadata)
+  
+      const uploadedText = await lighthouse.uploadText(metadataStringified,"LIGHTHOUSE APIKEY");
+      console.log('File Status:',uploadedText);
+    
+      const contractAddress = "0x280570B534FfC1C15f9b8486831fCC662a3FB09a"
+      const provider = new ethers.providers.AlchemyProvider("maticmum","ALCHEMYAPIKEY")
+      const signer = new ethers.Wallet("PRIVATEKEY", provider);
+      console.log("uploadedText",uploadedText)
+        
+      
+        let MyNFT = new ethers.Contract(contractAddress,NFTAbi,signer)
+        let Hash = `https://gateway.lighthouse.storage/ipfs/${uploadedText.data.Hash}`
+        console.log("Hash",Hash)
+        
+        let MintTransaction = await MyNFT.mintNFT(signer.getAddress(), Hash)
+        let recipt = await MintTransaction.wait()
+        console.log({recipt})
+        
+        setRecipt(recipt)
+        let recipts = {
+          recipt:recipt
+        } 
+        console.log("recipts",recipts)
+        
+        let reciptresult = JSON.stringify(recipts)
+        console.log("reciptresult",reciptresult)
+        setRecipt(recipt.transactionHash) 
+  } 
+    
+
+  return (
+    
+      <div>
+        <h1 className="slideInLeft">NFT MINTING</h1>
+        <div style={{background:"yellow",border:"5px solid black"}} className="App"> 
+          <form>
+          <input onChange={e => uploadImage(e)} style={{color:"black"}} type="file" />
+          <br></br>
+          <br></br>
+          <input style={{color : "black",background: "white",borderBlockColor:"black",borderBlockStyle:"solid",fontSize:"1.2em"}} value={name} type="text" placeholder='name...' onChange={handleChangeName}/>
+          <br></br>
+          <br></br>
+          <input style={{color : "black",background: "white",borderBlockColor:"black",borderBlockStyle:"solid",fontSize:"1.2em"}} value={des} type="text" placeholder='description...'onChange={handleChangeDes}/>
+          
+          <br></br>
+          <br></br>
+          <button  style={{fontStyle:"oblique",color : "white",background: "red",borderBlockColor:"black",fontSize:"1.40em",boxShadow:"1px 1px 1px green inset"}} onClick={(e) => changeSubmit(e)}>submit</button>
+            <div>
+              <h4 style={{color:"red",padding:"15px",background:"white",border:"10px solid black"}}>RESULT : {recipt}</h4>
+            </div>
+          </form> 
+        </div>
+      </div>
+    
+  );
+}
+
+export default App;
+
